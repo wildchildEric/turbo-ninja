26888c79b5a8931f169ef9148f195ee91ac24d98
lizhe <lizhe19851001@126.com>
2015-07-16 15:36:55 +0800

    init commit

---
+++ src/main/java/club/qiuwang/turboninja/Application.java
@@ -0,0 +1,22 @@
+package club.qiuwang.turboninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by lizhe on 15/7/16.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/club/qiuwang/turboninja/controllers/WelcomeController.java
@@ -0,0 +1,24 @@
+package club.qiuwang.turboninja.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Created by lizhe on 15/7/16.
+ */
+@Controller
+public class WelcomeController {
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "welcome";
+    }
+}
